9b3b4d2c013fbb50b7e58537a60c4c9c0573395f
Tarik Kilic <tarikkilic07@gmail.com>
2021-12-28 20:42:19 +0300

    Tutorial - Lesson 1.3  - High Speed Chase
    1. Add a speed variable for vehicle

--- Junior Programmer/Unit 1 - Player Control/Lesson 1.2 - Pedal to the Metal/Assets/Scripts/PlayerController.cs
+++ Junior Programmer/Unit 1 - Player Control/Lesson 1.2 - Pedal to the Metal/Assets/Scripts/PlayerController.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    // add a speed variable for vehicle, we can change the vehicle's speed
+    // allow it to be accessed from the inspector
+    public float speed = 5.0f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -16,6 +20,10 @@ public class PlayerController : MonoBehaviour
         // We'll move the vehicle forward
         //transform.Translate(0, 0, 1);
         //transform.Translate(Vector3.forward );
-        transform.Translate(Vector3.forward * Time.deltaTime * 20);
+        //transform.Translate(Vector3.forward * Time.deltaTime * 20);
+
+        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+
+        
     }
 }
