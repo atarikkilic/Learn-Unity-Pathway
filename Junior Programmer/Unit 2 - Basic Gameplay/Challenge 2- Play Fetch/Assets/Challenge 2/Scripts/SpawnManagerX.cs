5b286219d684b6f355741960fa950c84f12df317
Tarik Kilic <tarikkilic07@gmail.com>
2022-01-24 22:40:35 +0300

    Challenge 2 - Play Fetch
    3.Dogs are spawning at the top of the screen ✓
    
    4.The player is spawning green balls instead of dogs ✓
    
    5.The balls are destroyed if anywhere near the dog ✓
    
    6.Nothing is being destroyed off screen
    
    7.Only one type of ball is being spawned ✓
    
    8.Bonus: The spawn interval is always the same ✓

--- Junior Programmer/Unit 2 - Basic Gameplay/Challenge 2- Play Fetch/Assets/Challenge 2/Scripts/SpawnManagerX.cs
+++ Junior Programmer/Unit 2 - Basic Gameplay/Challenge 2- Play Fetch/Assets/Challenge 2/Scripts/SpawnManagerX.cs
@@ -6,9 +6,10 @@ public class SpawnManagerX : MonoBehaviour
 {
     public GameObject[] ballPrefabs;
 
-    private float spawnLimitXLeft = -22;
-    private float spawnLimitXRight = 7;
+    private float spawnLimitZLeft = 0;
+    private float spawnLimitZRight = 33;
     private float spawnPosY = 30;
+    private float spawnPosX = 22.58f;
 
     private float startDelay = 1.0f;
     private float spawnInterval = 4.0f;
@@ -16,17 +17,20 @@ public class SpawnManagerX : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        InvokeRepeating("SpawnRandomBall", startDelay, spawnInterval);
+        //Make the spawn interval a random value between 3 seconds and 5 seconds
+        InvokeRepeating("SpawnRandomBall", startDelay, Random.Range(spawnInterval - 1, spawnInterval + 1));
     }
 
-    // Spawn random ball at random x position at top of play area
+    // Spawn random ball at random z position at top of play area
     void SpawnRandomBall ()
     {
         // Generate random ball index and random spawn position
-        Vector3 spawnPos = new Vector3(Random.Range(spawnLimitXLeft, spawnLimitXRight), spawnPosY, 0);
+        Vector3 spawnPos = new Vector3(-spawnPosX, spawnPosY, Random.Range(spawnLimitZLeft, spawnLimitZRight));
 
         // instantiate ball at random spawn location
-        Instantiate(ballPrefabs[0], spawnPos, ballPrefabs[0].transform.rotation);
+        // Spawn ball randomly
+        int ballIndex = Random.Range(0, ballPrefabs.Length);
+        Instantiate(ballPrefabs[ballIndex], spawnPos, ballPrefabs[ballIndex].transform.rotation);
     }
 
 }
