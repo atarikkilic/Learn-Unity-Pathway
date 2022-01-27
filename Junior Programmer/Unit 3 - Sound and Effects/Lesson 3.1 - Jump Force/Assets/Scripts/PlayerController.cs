5a82e6389b2d1466f4bb5f06778bdae425321d1c
Tarik Kilic <tarikkilic07@gmail.com>
2022-01-27 22:44:40 +0300

    Tutorial - Lesson 3.1 - Jump Force
    6. Prevent player from double-jumping
    
    - Make the jump more realistic, we need an if-statement that makes sure the player is grounded before they jump
    
    7. Make an obstacle and move it left
    
    - From Course Library > Obstacles, add an obstacle, rename it “Obstacle”,  and position it where it should spawn
    
    - Apply a Rigid Body and Box Collider component, then edit the collider bounds to fit the obstacle
    
    - Create a new “Prefabs” folder and drag it in to create a new Original Prefab
    
    - Create a new “MoveLeft” script, apply it to the obstacle, and open it
    
    - In MoveLeft.cs, write the code to Translate it to the left according to the speed variable
    
    - Apply the MoveLeft script to the Background

--- Junior Programmer/Unit 3 - Sound and Effects/Lesson 3.1 - Jump Force/Assets/Scripts/PlayerController.cs
+++ Junior Programmer/Unit 3 - Sound and Effects/Lesson 3.1 - Jump Force/Assets/Scripts/PlayerController.cs
@@ -7,6 +7,7 @@ public class PlayerController : MonoBehaviour
     private Rigidbody playerRb;
     public float jumpForce = 10.0f;
     public float gravityModifier;
+    public bool isOnGround = true;
 
     // Start is called before the first frame update
     void Start()
@@ -18,12 +19,17 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space) && isOnGround)
         {
             //ForceMode.Impulse - Applies an instant force on the Object, taking mass into account.
             //This pushes the Object using the entire force in a single frame.
             playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            isOnGround = false;
         }
-        
+    }
+    // After jumping and returning to the ground, we set it to true with the collider
+    private void OnCollisionEnter(Collision collision)
+    {
+        isOnGround = true;
     }
 }
