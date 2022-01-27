0ca04154cbbddb3301951b4dc4ad9bdef96f1a9a
Tarik Kilic <tarikkilic07@gmail.com>
2022-01-27 22:04:16 +0300

    Tutorial - Lesson 3.1 - Jump Force
    4. Make player jump if spacebar pressed
    
    - Player only jump when we tell it to by pressing spacebar.
    
    5. Tweak the jump force and gravity
    
    - Replace the hardcoded value with a new public float jumpForce variable
    
    - We give the player a perfect jump by tweaking the force of the jump, the gravity of the scene, and the mass of the character.

--- Junior Programmer/Unit 3 - Sound and Effects/Lesson 3.1 - Jump Force/Assets/Scripts/PlayerController.cs
+++ Junior Programmer/Unit 3 - Sound and Effects/Lesson 3.1 - Jump Force/Assets/Scripts/PlayerController.cs
@@ -5,17 +5,25 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     private Rigidbody playerRb;
+    public float jumpForce = 10.0f;
+    public float gravityModifier;
 
     // Start is called before the first frame update
     void Start()
     {
         playerRb = GetComponent<Rigidbody>();
-        playerRb.AddForce(Vector3.up * 500);
+        Physics.gravity *= gravityModifier;
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            //ForceMode.Impulse - Applies an instant force on the Object, taking mass into account.
+            //This pushes the Object using the entire force in a single frame.
+            playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+        }
         
     }
 }
