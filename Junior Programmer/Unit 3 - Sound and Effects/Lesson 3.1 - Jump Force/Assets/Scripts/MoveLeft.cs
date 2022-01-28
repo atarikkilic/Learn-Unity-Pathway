36e2f5d21724217fd32ab0b7909fcf80a78abd4b
Tarik Kilic <tarikkilic07@gmail.com>
2022-01-28 22:54:10 +0300

    Lesson 3.2 - Make the World Whiz By
    4. Add a new game over trigger
    
    - When the player collides with an obstacle, we want to trigger a “Game Over”
    
    5. Stop MoveLeft on gameOver
    
    - We’ve added a gameOver bool that seems to work,  but the background and the objects continue to move when they collide with an obstacle. We need the MoveLeft script to communicate with the PlayerController, and stop once the player triggers gameOver.
    
    6. Stop obstacle spawning on gameOver
    
    - The background and the obstacles stop moving when gameOver == true, but the Spawn Manager is still raising an army of obstacles! We need to communicate with the Spawn Manager script and tell it to stop when the game is over.
    
    7. Destroy obstacles that exit bounds
    
    - We destroy any obstacles that exit boundaries

--- Junior Programmer/Unit 3 - Sound and Effects/Lesson 3.1 - Jump Force/Assets/Scripts/MoveLeft.cs
+++ Junior Programmer/Unit 3 - Sound and Effects/Lesson 3.1 - Jump Force/Assets/Scripts/MoveLeft.cs
@@ -5,15 +5,28 @@ using UnityEngine;
 public class MoveLeft : MonoBehaviour
 {
     private float speed = 30.0f;
+    // We can hold a reference to MoveLeft script to communicate with the PlayerController
+    private PlayerController playerControllerScript;
+
+    private float leftBound = -15;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerControllerScript = GameObject.Find("Player").GetComponent<PlayerController>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(Vector3.left * Time.deltaTime * speed);
+        if(playerControllerScript.gameOver == false)
+        {
+            transform.Translate(Vector3.left * Time.deltaTime * speed);
+        }
+        // The background will also disappear if we don't add the comparison tag with the "Obstacle".
+        if (transform.position.x < leftBound && gameObject.CompareTag("Obstacle"))
+        {
+            Destroy(gameObject);
+        }
     }
 }
--- Junior Programmer/Unit 3 - Sound and Effects/Lesson 3.1 - Jump Force/Assets/Scripts/PlayerController.cs
+++ Junior Programmer/Unit 3 - Sound and Effects/Lesson 3.1 - Jump Force/Assets/Scripts/PlayerController.cs
@@ -8,7 +8,7 @@ public class PlayerController : MonoBehaviour
     public float jumpForce = 10.0f;
     public float gravityModifier;
     public bool isOnGround = true;
-
+    public bool gameOver = false;
     // Start is called before the first frame update
     void Start()
     {
@@ -30,6 +30,15 @@ public class PlayerController : MonoBehaviour
     // After jumping and returning to the ground, we set it to true with the collider
     private void OnCollisionEnter(Collision collision)
     {
-        isOnGround = true;
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isOnGround = true;
+        }
+        else if (collision.gameObject.CompareTag("Obstacle"))
+        {
+            gameOver = true;
+            Debug.Log("Game Over !!!");
+        }
+        
     }
 }
