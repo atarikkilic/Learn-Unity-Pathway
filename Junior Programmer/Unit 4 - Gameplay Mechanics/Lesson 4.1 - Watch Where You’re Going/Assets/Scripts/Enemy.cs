40136f2024a96bc2d72b4af0db7142be45c2e80a
Tarik Kilic <tarikkilic07@gmail.com>
2022-02-01 22:13:48 +0300

    Lesson 4.2 - Follow the Player
    1. Add an enemy and a physics material
    
    - Create a new Sphere, rename it “Enemy” reposition it, and drag a texture onto it
    
    - Add a new RigidBody component and adjust its XYZ scale, then test
    
    - In a new “Physics Materials” folder, Create > Physics Material, then name it “Bouncy”
    
    - Increase the Bounciness to “1”, change Bounce Combine to “Multiply”,  apply it to your player and enemy, then test
    
    2. Create enemy script to follow player
    
    - Make a new “Enemy” script and attach it to the Enemy
    
    - In Update(), AddForce towards in the direction between the Player and the Enemy
    
    3. Create a lookDirection variable
    
    - In Update(), declare a new Vector3 lookDirection variable
    
    - Implement the lookDirection variable in the AddForce call

---
+++ Junior Programmer/Unit 4 - Gameplay Mechanics/Lesson 4.1 - Watch Where You’re Going/Assets/Scripts/Enemy.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public float speed = 3.0f;
+    private Rigidbody enemyRb;
+    private GameObject player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        enemyRb = GetComponent<Rigidbody>();
+        player = GameObject.Find("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Enemy goes toward up to player, always goes constant speed regardless of distance with .normalized
+        // Normalize that entire vector,so that way it doesn't increase in magnitude the farther the two objects are away
+        Vector3 lookDirection = (player.transform.position - transform.position).normalized;
+        enemyRb.AddForce(lookDirection * speed);
+    }
+}
