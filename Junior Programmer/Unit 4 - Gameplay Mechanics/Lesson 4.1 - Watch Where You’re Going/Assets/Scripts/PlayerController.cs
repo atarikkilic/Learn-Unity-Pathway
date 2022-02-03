b69bb3308776449358278902888985e44c5c9ff1
Tarik Kilic <tarikkilic07@gmail.com>
2022-02-03 22:21:37 +0300

    Lesson 4.3 - PowerUp and CountDown
    1. Choose and prepare a powerup
    
    - From the Library, drag a Powerup object into the scene, rename it “Powerup” and edit its scale & position
    
    - Add a Box Collider to the powerup, click Edit Collider to make sure it fits, then check the “Is Trigger” checkbox
    
    - Create a new “Powerup” tag and apply it to the powerup
    
    - Drag the Powerup into the Prefabs folder to create a new “Original Prefab”
    
    2. Destroy powerup on collision
    
    - In PlayerController.cs, add a new OnTriggerEnter() method
    
    - Add an if-statement that destroys other.CompareTag("Powerup") powerup on collision
    
    - Create a new public bool hasPowerup; and set hasPowerup = true;  when you collide with the Powerup

--- Junior Programmer/Unit 4 - Gameplay Mechanics/Lesson 4.1 - Watch Where You’re Going/Assets/Scripts/PlayerController.cs
+++ Junior Programmer/Unit 4 - Gameplay Mechanics/Lesson 4.1 - Watch Where You’re Going/Assets/Scripts/PlayerController.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public bool hasPowerup = false;
     private Rigidbody playerRb;
     private GameObject focalPoint;
     public float speed = 5.0f;
@@ -20,5 +21,14 @@ public class PlayerController : MonoBehaviour
         float forwardInput = Input.GetAxis("Vertical");
         playerRb.AddForce(focalPoint.transform.forward * forwardInput * speed);
         //playerRb.AddForce(Vector3.forward * forwardInput * speed);
+    }
+    // Destroy the power-up when player collected the power-up
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Powerup"))
+        {
+            hasPowerup = true;
+            Destroy(other.gameObject);
+        }
     }
 }
