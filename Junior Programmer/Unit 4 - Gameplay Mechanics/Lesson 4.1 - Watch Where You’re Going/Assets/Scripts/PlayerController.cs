f474b7037023d27188653bcd3dfef1a4b399696a
Tarik Kilic <tarikkilic07@gmail.com>
2022-02-03 22:45:49 +0300

    Lesson 4.3 - PowerUp and CountDown
    3. Test for enemy and powerup
    
    - Create a new “Enemy” tag and apply it to the Enemy Prefab
    
    - In PlayerController.cs, add the OnCollisionEnter() function
    
    - Create the if-statement with the double-condition testing for enemy tag and hasPowerup boolean
    
    - Create a Debug.Log to make sure it’s working
    
    4. Apply extra knockback with powerup
    
    -In OnCollisionEnter() declare a new local variable to get the Enemy’s Rigidbody component
    
    - Declare a new variable to get the direction away from the player
    
    - Add an impulse force to the enemy, using a new powerupStrength variable

--- Junior Programmer/Unit 4 - Gameplay Mechanics/Lesson 4.1 - Watch Where You’re Going/Assets/Scripts/PlayerController.cs
+++ Junior Programmer/Unit 4 - Gameplay Mechanics/Lesson 4.1 - Watch Where You’re Going/Assets/Scripts/PlayerController.cs
@@ -7,6 +7,7 @@ public class PlayerController : MonoBehaviour
     public bool hasPowerup = false;
     private Rigidbody playerRb;
     private GameObject focalPoint;
+    private float powerupStrength = 15.0f;
     public float speed = 5.0f;
     // Start is called before the first frame update
     void Start()
@@ -29,6 +30,20 @@ public class PlayerController : MonoBehaviour
         {
             hasPowerup = true;
             Destroy(other.gameObject);
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.CompareTag("Enemy") && hasPowerup)
+        {
+            Rigidbody enemyRb = collision.gameObject.GetComponent<Rigidbody>();
+            // Suppose that enemy pos = (1,0,1) and player pos = (1,0,2)
+            // awayFromPlayer = (0,0,-1) and going this way
+            Vector3 awayFromPlayer = (collision.gameObject.transform.position - transform.position);
+            enemyRb.AddForce(awayFromPlayer * powerupStrength, ForceMode.Impulse); // add an instant force impulse to the rigidbody
+
+            Debug.Log("Collided with: " + collision.gameObject.name + " with powerup set to " + hasPowerup);
         }
     }
 }
