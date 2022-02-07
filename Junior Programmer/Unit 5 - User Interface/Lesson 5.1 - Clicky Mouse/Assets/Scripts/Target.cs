61f95276c0ace5f544de8ea3fb8fd664435b0ef6
Tarik Kilic <tarikkilic07@gmail.com>
2022-02-07 22:20:55 +0300

    Lesson 5.1 - Clicky Mouse
    4. Replace messy code with new methods
    
    5. Create object list in Game Manager
    
    - Create a new “Game Manager” Empty object, attach a new GameManager.cs script, then open it
    
    - Declare a new public List<GameObject> targets;, then in the Game Manager inspector, change the list Size to 4 and assign your prefabs
    
    6. Create a coroutine to spawn objects
    
    - Declare and initialize a new private float spawnRate variable
    
    - Create a new IEnumerator SpawnTarget () method
    
    - Inside the new method, while(true), wait 1 second, generate a random index,  and spawn a random target
    
    - In Start(), use the StartCoroutine method to begin spawning objects
    
    7. Destroy target with click and sensor
    
    - In Target.cs, add a new method for private void OnMouseDown() { } , and inside that method, destroy the gameObject
    
    - Add a new method for private void OnTriggerEnter(Collider other) and inside that function, destroy the gameObject

--- Junior Programmer/Unit 5 - User Interface/Lesson 5.1 - Clicky Mouse/Assets/Scripts/Target.cs
+++ Junior Programmer/Unit 5 - User Interface/Lesson 5.1 - Clicky Mouse/Assets/Scripts/Target.cs
@@ -5,22 +5,54 @@ using UnityEngine;
 public class Target : MonoBehaviour
 {
     private Rigidbody targetRb;
+    private float minSpeed = 12;
+    private float maxSpeed = 16;
+    private float maxTorque = 10;
+    private float xRange = 4;
+    private float ySpawnPos = -6;
 
     // Start is called before the first frame update
     void Start()
     {
         targetRb = GetComponent<Rigidbody>();
 
-        targetRb.AddForce(Vector3.up * Random.Range(12, 16), ForceMode.Impulse);
+        targetRb.AddForce(RandomForce(), ForceMode.Impulse);
         // Torque will apply the force for our objects to rotate
-        targetRb.AddTorque(Random.Range(-10,10), Random.Range(-10, 10), Random.Range(-10, 10), ForceMode.Impulse);
+        targetRb.AddTorque(RandomTorque(), RandomTorque(), RandomTorque(), ForceMode.Impulse);
         // Get the position of our current object and we'll set this to be a new random position
-        transform.position = new Vector3(Random.Range(-4, 4), -6);
+        transform.position = RandomSpawnPos();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    // When mouse button 1 is clicked destroy the objects
+    private void OnMouseDown()
+    {
+        Destroy(gameObject);
+    }
+
+    // When one of our objects enters the trigger for that sensor, we can destroy it.
+    private void OnTriggerEnter(Collider other)
+    {
+        Destroy(gameObject);
+    }
+
+    Vector3 RandomForce()
+    {
+        return Vector3.up * Random.Range(minSpeed, maxSpeed);
+    }
+
+    float RandomTorque() 
+    {
+        return Random.Range(-maxTorque, maxTorque);
+    }
+
+    Vector3 RandomSpawnPos()
+    {
+        return new Vector3(Random.Range(-xRange, xRange), ySpawnPos);
     }
 }
