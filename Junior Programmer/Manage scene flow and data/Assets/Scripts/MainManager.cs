7590743cefc0004fd570203565866f4fd14f9ad1
Tarik Kilic <tarikkilic07@gmail.com>
2022-02-20 23:35:09 +0300

    Implement data persistence between sessions
    3. Add a LoadColor method
    
    4. Load and save the color in the application
    
    5. Add testing functionality

--- Junior Programmer/Manage scene flow and data/Assets/Scripts/MainManager.cs
+++ Junior Programmer/Manage scene flow and data/Assets/Scripts/MainManager.cs
@@ -22,6 +22,8 @@ public class MainManager : MonoBehaviour
 
         Instance = this;
         DontDestroyOnLoad(gameObject);
+
+        LoadColor();
     }
 
     [System.Serializable]
@@ -40,4 +42,18 @@ public class MainManager : MonoBehaviour
 
         File.WriteAllText(Application.persistentDataPath + "/savefile.json", json);
     }
+
+
+    public void LoadColor()
+    {
+        string path = Application.persistentDataPath + "/savefile.json";
+        if (File.Exists(path))
+        {
+            string json = File.ReadAllText(path);
+            SaveData data = JsonUtility.FromJson<SaveData>(json);
+
+            TeamColor = data.TeamColor;
+        }
+    }
+
 }
